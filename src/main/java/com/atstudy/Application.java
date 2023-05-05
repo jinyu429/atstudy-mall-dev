687c7f29e9804a2db70b160889bfe9cf412b4c6b
justbeme <JustBeMe98@outlook.com>
2023-05-05 18:05:45 +0800

    2023/5/5:第二次提交，基础框架搭建好，登录功能可以实现了。

--- src/main/java/com/atstudy/Application.java
+++ src/main/java/com/atstudy/Application.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class Application {
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class,args);
     }
 }
