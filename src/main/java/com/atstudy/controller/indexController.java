b2d5e6497ff3097d0b0f64692794373e2b468237
justbeme <JustBeMe98@outlook.com>
2023-05-05 13:16:49 +0800

    首次提交，框架搭建之前

---
+++ src/main/java/com/atstudy/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.atstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/","/index"})
+public class indexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
